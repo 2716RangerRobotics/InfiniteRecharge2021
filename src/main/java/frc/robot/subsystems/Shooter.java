5fc1c9ee72ea044695a0100514ab4ef6e2f041f2
Rob Heslin <Rob.Heslin@gmail.com>
2021-02-01 19:10:52 -0500

    Added comments to shooter class, and removed extra commands from RobotConatinter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,14 +4,17 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+/**
+ * A one wheel shooter class
+ */
 public class Shooter extends SubsystemBase {
   TalonSRX shooterWheel;
   /** Creates a new Shooter. */
@@ -21,7 +24,10 @@ public class Shooter extends SubsystemBase {
     shooterWheel.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
     shooterWheel.configVoltageCompSaturation(12.5);
     shooterWheel.enableVoltageCompensation(true);
+    
+    //this needs to be true if wheels is spinning the wrong way
     shooterWheel.setInverted(false);
+    //if wheel spin is bawards from sensor, this must be true
     shooterWheel.setSensorPhase(false);
     shooterWheel.setNeutralMode(NeutralMode.Coast);
     shooterWheel.config_kP(0, 1.0);
@@ -37,12 +43,26 @@ public class Shooter extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  /**
+   * returns the speed of the wheel
+   * @return this is in native units, per 100ms
+   */
   public double getSpeed(){
     return shooterWheel.getSelectedSensorVelocity();
   } 
+
+  /**
+   * set the speed of the wheel
+   * @param speed this speed is in native units, per 100ms
+   */
   public void setSpeed(double speed){
     shooterWheel.set(ControlMode.Velocity, speed);
   }
+
+  /**
+   * This method sets the shooter motor to zero volts in percent output
+   */
   public void stop(){
     shooterWheel.set(ControlMode.PercentOutput, 0.0);
   }
